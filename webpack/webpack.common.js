dc76ccefc49fdce48a4653b2f11734e6866fd973
Hendrik Mans <hendrik@mans.de>
2020-04-01 13:26:45 +0200

    Add typed CSS module support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { TypedCssModulesPlugin } = require("typed-css-modules-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -13,7 +14,18 @@ module.exports = {
   module: {
     rules: [
       { test: /\.tsx?$/, use: "ts-loader" },
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] }
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
@@ -24,6 +36,10 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new TypedCssModulesPlugin({
+      globPattern: "src/**/*.css",
+      camelCase: true
+    }),
     new CopyPlugin([{ from: "assets", to: "assets" }]),
     new HtmlWebpackPlugin({
       template: "src/index.html",
