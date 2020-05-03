6f745ed6a4c2f2699a634fd5184faf69d814333e
Hendrik Mans <hendrik@mans.de>
2020-05-03 10:55:14 +0200

    Add a global CSS entry point

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+/* Let's import some global CSS for those cases where we don't want to (or
+can't) use CSS Modules. */
+import "./css/global.css";
+
+/* Your code here! */
 const message: string = "Insert your TypeScript code here!";
 console.log(message);
